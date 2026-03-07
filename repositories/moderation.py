87481ddcc971bb2367e956603121afe3db8b2649
marulyanova <devchatay@gmail.com>
2026-03-07 14:56:57 +0300

    add hw6, prometheus, grafana

--- repositories/moderation.py
+++ repositories/moderation.py
@@ -1,6 +1,8 @@
+import time
 from dataclasses import dataclass
 from typing import Mapping, Any
 from clients.postgres import get_pg_connection
+from metrics import DB_QUERY_DURATION
 
 
 class ModerationResultNotFoundError(Exception):
@@ -15,19 +17,30 @@ class ModerationResultStorage:
             VALUES ($1, 'pending')
             RETURNING *
         """
+        start = time.time()
         async with get_pg_connection() as conn:
             row = await conn.fetchrow(query, item_id)
-            return dict(row)
+        duration = time.time() - start
+        DB_QUERY_DURATION.labels(
+            query_type="insert", table="moderation_results"
+        ).observe(duration)
+        return dict(row)
 
     async def get_by_id(self, task_id: int) -> Mapping[str, Any]:
         query = """
             SELECT * FROM moderation_results WHERE id = $1
         """
+        start = time.time()
         async with get_pg_connection() as conn:
             row = await conn.fetchrow(query, task_id)
-            if not row:
-                raise ModerationResultNotFoundError()
-            return dict(row)
+        duration = time.time() - start
+        DB_QUERY_DURATION.labels(
+            query_type="select", table="moderation_results"
+        ).observe(duration)
+
+        if not row:
+            raise ModerationResultNotFoundError()
+        return dict(row)
 
 
 @dataclass(frozen=True)
